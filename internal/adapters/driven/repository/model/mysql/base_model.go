f82726ca28dbf8ba8b63155da68c294f6772c992
Alireza Farhanian <aariow01@gmail.com>
2024-07-17 01:29:30 +0330

    chore: change id type for base-model

--- internal/adapters/driven/repository/model/mysql/base_model.go
+++ internal/adapters/driven/repository/model/mysql/base_model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        uint `gorm:"primaryKey"`
+	ID        uint64 `gorm:"primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
